f784ee9b0f322b33ac622812119920ad8f593295
Bipin Manandhar <kubipin07@gmail.com>
2018-06-26 17:26:42 +0545

    Make the slider continuous

--- ast3/script.js
+++ ast3/script.js
@@ -4,8 +4,8 @@ var previous_btn = document.getElementsByTagName('button')[0];
 var next_btn = document.getElementsByTagName('button')[1];
 
 var min_limit = 0;
-var margin = 0;
-var max_limit = -8400;
+var margin = -1400;
+var max_limit = -11200;
 var factor = 20;
 
 
@@ -20,6 +20,11 @@ function animate(left_translation){
     var id = setInterval(function() {
         if (margin % 1400 === 0){
             clearInterval(id);
+            if (margin == 0){
+                margin = -9800;
+            } else if (margin == -11200){
+                margin = -1400;
+            }
         } else {
             if(left_translation && margin >= max_limit){
                 // next_button clicked
@@ -27,11 +32,12 @@ function animate(left_translation){
             } else if (!left_translation && margin <= min_limit){
                 // previous button clicked
                 margin += factor;
-            } else if (margin > max_limit){
-                margin -= factor;
-            } else {
-                margin += factor;
             }
+            // } else if (margin > max_limit){
+            //     margin -= factor;
+            // } else {
+            //     margin += factor;
+            // }
             slide.style.marginLeft = margin.toString() + "px";
         }
     }, 1);
