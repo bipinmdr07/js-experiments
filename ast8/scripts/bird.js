ba33db6b99537ea6a34766093c78508d2750d1bb
Bipin Manandhar <kubipin07@gmail.com>
2018-07-04 12:12:59 +0545

    Add collision detection between wall and bird

--- ast8/scripts/bird.js
+++ ast8/scripts/bird.js
@@ -2,11 +2,15 @@ class Bird {
     constructor(props) {
         this.x = props.x;
         this.y = props.y;
+        this.width = props.width;
+        this.height = props.height;
         this.xVelocity = 1;
-        this.yVelocity = 0;
-        this.gravity = 0.2;
+        this.yVelocity = 5;
+        this.gravity = 10;
         this.time = 0;
+        this.$bird = props.$bird;
         this.$element = props.$element;
+        this.theta = Math.atan(this.yVelocity / this.xVelocity);
 
         this.init();
     }
@@ -17,16 +21,39 @@ class Bird {
     }
 
     updateBirdPosition() {
-        this.time += 0.1;
-        this.y = (-this.yVelocity * this.time + 1 / 2 * this.gravity * this.time * this.time);
-        // this.y += 0.1;
+        this.time += 0.01;
+        let netAddingTerm = - this.yVelocity * this.time + 1 / 2 * this.gravity * this.time * this.time;
+        this.y += netAddingTerm;
+
+        if (this.y < 0) {
+            this.y = 0;
+        }
+
+        if (this.y + this.width > 600) {
+            this.y = 600 - this.width;
+        }
+        this.theta = Math.atan(netAddingTerm / this.xVelocity) * 180 / Math.PI;
+
         this.$element.style.top = this.y + 'px';
         this.$element.style.left = this.x + 'px';
+        this.$bird.style.transform = "rotate(" + this.theta + "deg)";
     }
 
     setUpwardAcceleration() {
-        console.log(this.y);
         this.yVelocity = 5;
-        this.time = 0;
+        this.time = 0.00;
+    }
+
+    checkCollision(walls) {
+        let topWall = walls[0];
+        let bottomWall = walls[1];
+
+        if (this.x + this.width > topWall.x && this.y < topWall.y + topWall.height && this.x < topWall.x + topWall.width
+            || this.x + this.width > bottomWall.x && this.y + this.width > bottomWall.y && this.x < bottomWall.x + bottomWall.width){
+            console.log("google");
+            return true;
+        }
+
+        return false;
     }
 }
--- ast8/scripts/game-controller.js
+++ ast8/scripts/game-controller.js
@@ -1,90 +1,112 @@
-const $mainWrappper = document.getElementById('main-wrapper');
-const $homeScreen = document.getElementById('home-screen');
-const $startBtn = document.getElementById('start-btn');
-const $gameScreen = document.getElementById('game-screen');
-const $gameOverScreen = document.getElementById('gameover-screen');
-const $bird = document.getElementsByClassName('bird')[0];
+// const $mainWrappper = document.getElementById('main-wrapper');
+// const $homeScreen = document.getElementById('home-screen');
+// const $startBtn = document.getElementById('start-btn');
+// const $gameScreen = document.getElementById('game-screen');
+// const $gameOverScreen = document.getElementById('gameover-screen');
+// const $bird = document.getElementsByClassName('bird')[0];
 
-const WALL_SPACE = 125;
+// const WALL_SPACE = 125;
 
 class GameController {
     constructor(props) {
         this.startGameLoop = false;
         this.walls = [];
+        this.wallSpace = 125;
         this.init();  
     }
 
     init() {
         this.homeScreen = new HomeScreen({
-            $element: $homeScreen
+            $element: this.getHomeScreen()
         });
 
         this.gameScreen = new GameScreen({
-            $element: $gameScreen
+            $element: this.getGameScreen()
         });
 
         this.gameOverScreen = new GameOverScreen({
-            $element: $gameOverScreen
+            $element: this.getGameOverScreen()
         });
 
         this.bird = new Bird({
             x: 20,
             y: 250,
-            $element: $bird
+            height: 30,
+            width: 30,
+            $element: this.getBorderBox(),
+            $bird: this.getBird()
         });
 
         this.startGame();
+
+        window.onkeydown = (e) => {
+            let key = e.keyCode ? e.keyCode : e.which;
+
+            if (key === 32 && !this.gamePaused) {
+                this.bird.setUpwardAcceleration();
+            }
+        }
     }
 
     startGame() {
-        $startBtn.onclick = () => {
+        this.getStartBtn().onclick = () => {
             this.homeScreen.hideHomeScreen();
             this.gameScreen.showGameScreen();
 
             this.startGameLoop = true;
+            this.gamePaused = false;
         }
 
         let id = setInterval(() => {
-            if (this.startGameLoop) {
-                // console.log(this.walls);
+            if (!this.gamePaused) {
+
+                this.bird.updateBirdPosition();
 
                 if (this.walls.length === 0 || this.walls[this.walls.length - 1].x < 600) {
                     this.generateObstacles();
                 }
-
-                let indices = [];
 
                 this.walls.map((wall) => {
                     wall.updateWallPosition();
                     wall.renderWall();
 
                     if (wall.x < -50) {
-                        indices.push(this.walls.indexOf(wall));
                         wall.destroyWall();
                         this.walls.splice(this.walls.indexOf(wall), 1);
                     }
                 });
-            }
-        }, 10);
-        // this.generateObstacles();
 
-        // clearInterval(id);
+
+            }
+
+            if (this.gamePaused) {
+                clearInterval(id);
+            }
+
+            if (this.bird.checkCollision(this.walls)){
+                this.gamePaused = true;
+                console.log("hello");
+            }
+
+            // if (!this.startGameLoop){
+            //     clearInterval(id);
+            // }
+        }, 10);
     }
 
     generateObstacles() {
         let upperWallHeight = Math.floor(Math.random() * 300) + 100;
-        let lowerWallHeight = 600 - upperWallHeight - WALL_SPACE;
-        let lowerWallY = upperWallHeight + WALL_SPACE;
+        let lowerWallHeight = 600 - upperWallHeight - this.wallSpace;
+        let lowerWallY = upperWallHeight + this.wallSpace;
         let upperWall = new UpperWall({
             x: 1000,
             y: 0,
             velocity: 1,
             width: 50,
             height: upperWallHeight,
-            $parent: $gameScreen 
+            $parent: this.getGameScreen()
         });
 
-        console.log(this);
         upperWall.pushSelfToWalls(this.walls);
 
         let lowerWall = new LowerWall({
@@ -93,18 +115,18 @@ class GameController {
             velocity: 1,
             width: 50,
             height: lowerWallHeight,
-            $parent: $gameScreen
+            $parent: this.getGameScreen()
         });
 
         lowerWall.pushSelfToWalls(this.walls);
     }
 
     pauseGame() {
-
+        this.gamePaused = true;
     }
 
     resumeGame() {
-
+        this.gamePaused = false;
     }
 
     restartGame() {
@@ -113,6 +135,39 @@ class GameController {
 
     exitGame() {
 
+    }
+
+    // getters
+    getMainWrapper() {
+        return document.getElementById('main-wrapper');
+    }
+
+    getHomeScreen() {
+        return document.getElementById('home-screen');
+    }
+
+    // const $startBtn = document.getElementById('start-btn');
+    getStartBtn() {
+        return document.getElementById('start-btn');
+    }
+
+    // const $gameScreen = document.getElementById('game-screen');
+    getGameScreen() {
+        return document.getElementById('game-screen');
+    }
+
+    // const $gameOverScreen = document.getElementById('gameover-screen');
+    getGameOverScreen() {
+        return document.getElementById('gameover-screen');
+    }
+
+    // const $bird = document.getElementsByClassName('bird')[0];
+    getBird() {
+        return document.getElementsByClassName('bird')[0];
+    }
+
+    getBorderBox() {
+        return document.getElementsByClassName('border-box')[0];
     }
 }
 
